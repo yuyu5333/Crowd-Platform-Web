0e619eb366229614bef18adab3f03e0b5b237f9d
unknown <1924582937@qq.com>
2023-03-11 23:43:14 +0800

    3.11

---
+++ iview-admin-master/src/view/ModelCompress/SystemModel.vue
@@ -0,0 +1,236 @@
+<template>
+  <div>
+    <div>
+      <card shadow>
+        <Row>
+          <Col span="2">
+            <p class="head-font">设备选择：</p>
+          </Col>
+          <Col span="6">
+            <Select v-model="device" @on-change="checkDeviceChange" style="width:100px;margin-right: 20px">
+              <Option v-for="item in deviceList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Divider style="margin: 12px 0px"/>
+        <Row>
+          <Col span="6">设备名:
+            <p class="text-font">{{ deviceStatus.DeviceName }}</p>
+          </Col>
+          <Col span="6">电池容量(MAh):
+            <p class="text-font">{{ deviceStatus.BatteryVolume }}</p>
+          </Col>
+          <Col span="6">CPU:
+            <p class="text-font">{{ deviceStatus.CPU }}</p>
+          </Col>
+          <Col span="6">DRAM(GB):
+            <p class="text-font">{{ deviceStatus.DRam }}</p>
+          </Col>
+        </Row>
+      </card>
+      <div>
+        <br>
+      </div>
+      <card shadow>
+        <Row>
+          <Col span="2">
+            <p class="head-font">任务选择：</p>
+          </Col>
+          <Col span="6">
+            <Cascader :data="data" v-model="mission"
+                      style="width:150px" @on-change="checkMissionChange"></Cascader>
+          </Col>
+        </Row>
+        <Divider style="margin: 12px 0px"/>
+        <Row>
+          <Col span="4">计算量(MFlops):
+            <p class="text-font">{{ missionStatus.Flops }}</p>
+          </Col>
+          <Col span="4">参数量(MParams):
+            <p class="text-font">{{ missionStatus.Params }}</p>
+          </Col>
+          <Col span="4">存储量(MB):
+            <p class="text-font">{{ missionStatus.Storage }}</p>
+          </Col>
+          <Col span="4">能耗(mJ):
+            <p class="text-font">{{ missionStatus.Energy }}</p>
+          </Col>
+          <Col span="4">精度(%):
+            <p class="text-font">{{ missionStatus.Accuracy }}</p>
+          </Col>
+        </Row>
+      </card>
+    </div>
+    <div>
+      <br>
+    </div>
+    <div>
+      <card size="large">
+        <p class="head-font" style="color: rgb(17,75,218)">模型压缩</p>
+        <Divider style="margin: 12px 0px"/>
+        <Row>
+          <Col span="4" class="head-font">
+            拖动以选择压缩率：
+          </Col>
+          <Col span="10">
+            <Slider v-model="compressRate" style="margin-right: 40px" show-input ></Slider>
+          </Col>
+          <Col span="10">
+            <Button @click="compressModel" type="primary">开始压缩</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Row style="margin: 20px">
+            <Col span="3">计算量(MFlops):
+              <p class="result">{{ compressStatus.Flops }}</p>
+            </Col>
+            <Col span="5">
+              与压缩前之比：
+              <p class="compare">{{ (compressStatus.Flops / missionStatus.Flops).toFixed(3) }}</p>
+            </Col>
+            <Col span="3">参数量(MParams)：
+              <p class="result">{{ compressStatus.Params }}</p>
+            </Col>
+            <Col span="5">
+              与压缩前之比：
+              <p class="compare">{{ (compressStatus.Params / missionStatus.Params).toFixed(3) }}</p>
+            </Col>
+            <Col span="3">存储量(MB)：
+              <p class="result">{{ compressStatus.Storage }}</p>
+            </Col>
+            <Col span="5">
+              与压缩前之比：
+              <p class="compare">{{ (compressStatus.Storage / missionStatus.Storage).toFixed(3) }}</p>
+            </Col>
+          </Row>
+          <Row style="margin: 20px 20px;">
+            <Col span="3">能耗(mJ)：
+              <p class="result">{{ compressStatus.Energy }}</p>
+            </Col>
+            <Col span="5">
+              与压缩前之比：
+              <p class="compare">{{ (compressStatus.Energy / missionStatus.Energy).toFixed(3) }}</p>
+            </Col>
+            <Col span="3">精度(%)：
+              <p class="result">{{ compressStatus.Accuracy }}</p>
+            </Col>
+            <Col span="5">
+              与压缩前之比：
+              <p class="compare">{{ (compressStatus.Accuracy / missionStatus.Accuracy).toFixed(3) }}</p>
+            </Col>
+          </Row>
+          <p class="head-font" style="margin-left: 35%;margin-bottom: 100px">
+            选用的压缩算子为：{{ compressStatus.ModelName }}
+          </p>
+          <Button type="primary" style="margin-left: 45%">
+            <a :href="'download-model/?model='+ compressStatus.ModelName" style="color: white">下载模型 </a>
+          </Button>
+        </Row>
+      </card>
+    </div>
+
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      mission: [],
+      data: [{
+        value: 'image_classification',
+        label: '图像分类',
+        children: [
+          {
+            value: 'Vgg16',
+            label: 'Vgg16'
+          },
+          {
+            value: 'AlexNet',
+            label: 'AlexNet'
+          }
+        ]
+      }, {
+        value: 'semantic_segmentation',
+        label: '语义分割',
+        disabled: true,
+        children: []
+      }],
+      deviceList: [
+        {
+          value: 'Xiaomi 12',
+          label: 'Xiaomi 12'
+        },
+        {
+          value: 'Iphone 14',
+          label: 'Iphone 14'
+        }
+      ],
+      device: [],
+      compressRate: 75,
+      deviceStatus: [],
+      missionStatus: [],
+      compressStatus: []
+    }
+  },
+  mounted() {
+  },
+  watch: {},
+  methods: {
+    checkDeviceChange(device) {
+      let that = this;
+      axios.post('get-device/', {
+        DeviceName: device
+      })
+        .then(response => (that.deviceStatus = response.data))
+    },
+    checkMissionChange(mission) {
+      let that = this;
+      axios.post('get-mission/', {
+        MissionName: mission
+      })
+        .then(response => {
+          console.log(response.data);
+          that.missionStatus = response.data
+        })
+    },
+    compressModel() {
+      let that = this;
+      axios.post('compress-model/', {
+        CompressRate: this.compressRate,
+        MissionName: this.mission
+      })
+        .then(response => {
+          console.log(response.data);
+          that.compressStatus = response.data
+        })
+    },
+  }
+}
+</script>
+
+<style>
+.head-font {
+  font-weight: bolder;
+  font-size: 20px;
+}
+
+.result {
+  font-weight: bold;
+  font-size: 17px;
+  color: indianred;
+}
+
+.compare {
+  font-weight: bold;
+  font-size: 17px;
+  color: green;
+}
+
+.text-font {
+  font-weight: bold;
+  font-size: 17px;
+  color: #6b25d3;
+}
+</style>
---
+++ iview-admin-master/src/view/hmthome/hmthome.vue
@@ -0,0 +1,240 @@
+<template>
+  <div
+    class="background-image"
+    :style="{ backgroundImage: 'url(' + backgroundImageUrl + ')' }"
+  >
+    <!-- <div> -->
+    
+    <!-- <div class="iamge-container"> -->
+    
+
+    <div style="text-align:center;" class="hover-animation">
+      <a href="http://www.crowdhmt.com/">
+        <img src="/images/hmtfirstpage/zonglan2.png" alt="your image" style="vertical-align:middle;" 
+        v-b-tooltip.hover.top="imageDescription.firstinfo"
+        class="small-image" />
+      </a>
+    </div>
+
+    <div class="image-container">
+      <row class="row">
+        <router-link tag="div" class="image" to="/cog_natural/cog_natural">
+          <img
+            src="/images/ziyuanganzhi.png"
+            alt="Image 1"
+            class="hover-animation"
+            v-b-tooltip.hover.top="imageDescription.deviceinfo"
+          />
+        </router-link>
+        <div class="text">
+          <h2 class="bold-header" > 
+            <a
+              href="http://blog.crowdhmt.com/articles/embedded_Intelligence"
+              target="_blank"
+              style=" background-color: rgba(255, 255, 255, 0.8);"
+              >设备资源感知</a
+            >
+          </h2>
+          <!-- <p>支持为用户上传模型定制模型压缩方案，缩减模型体量以满足设定资源</p> -->
+          <p></p>
+
+          <p>支持于多种智能设备以及资源受限平台上(支持MCU、树莓派、FPGA、</p>
+          <p>GPUnano、手机5种设备实时资源感知)</p>
+
+        </div>
+      </row>
+
+      <row class="row">
+        <router-link
+          tag="div"
+          class="image"
+          to="/ModelPerformaceEvaluation/firstpage"
+        >
+          <img
+            src="/images/moxingganzhi.png"
+            alt="Image 2"
+            class="hover-animation"
+            v-b-tooltip.hover.top="imageDescription.modelinfo"
+          />
+        </router-link>
+        <div class="text">
+          <h2 class="bold-header">
+            <a
+              href="http://blog.crowdhmt.com/articles/mechanism"
+              target="_blank"
+              style=" background-color: rgba(255, 255, 255, 0.8);"
+              >模型性能评估</a
+            >
+          </h2>
+          <!-- <p>支持为用户上传模型定制模型压缩方案，缩减模型体量以满足设定资源</p> -->
+          <p></p>
+          <p>支持用户自定义模型上传，评估硬件相关性能（能耗、时延），权重相</p>
+          <p>关性能（精度），模型结构相关性能（计算量、存储量）</p>
+
+          
+        </div>
+      </row>
+
+      <row class="row">
+        <router-link tag="div" class="image" to="/ModelCompress/ModelCompress">
+          <img
+            src="/images/yasuo.png"
+            alt="Image 3"
+            class="hover-animation"
+            v-b-tooltip.hover.top="imageDescription.modelcompress"
+          />
+        </router-link>
+        <div class="text">
+          <h2 class="bold-header">
+            <a
+              href="http://blog.crowdhmt.com/articles/model_compression"
+              target="_blank"
+              style=" background-color: rgba(255, 255, 255, 0.8);"
+              >端自适应模型压缩</a
+            >
+          </h2>
+          <p></p>
+          <p>支持为用户上传模型定制模型压缩方案，缩减模型体量以满足设定资源</p>
+          <p>为了在计算资源有限的设备上部署模型，例如移动设备和嵌入式系统等</p>
+        </div>
+      </row>
+
+      <row class="row">
+        <router-link tag="div" class="image" to="/segmentation/segmentation">
+          <img
+            src="/images/fenge.png"
+            alt="Image 3"
+            class="hover-animation"
+            v-b-tooltip.hover.top="imageDescription.mdoelseg"
+          />
+        </router-link>
+        <div class="text">
+          <h2 class="bold-header">
+            <a
+              href="http://blog.crowdhmt.com/articles/model_partition"
+              target="_blank"
+              style=" background-color: rgba(255, 255, 255, 0.8);"
+
+              >端自适应模型分割</a
+            >
+          </h2>
+          <p></p>
+     <!-- <p>支持为用户上传模型定制模型压缩方案，缩减模型体量以满足设定资源</p> -->
+          <p>支持将一个大型的深度学习模型划分成多个部分，并将这些部分分别部</p>
+          <p>署到不同的设备上进行计算，从而提高深度学习模型的性能，并降低运</p>
+          <p>行成本</p>
+        </div>
+      </row>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { VBTooltip } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {
+  data() {
+    return {
+      showDescription: false,
+      imageDescription: {
+        firstinfo: '欢迎来到人机物融合群智计算系统开放平台，为您提供一个从设备到模型部署的中间件服务（点击进入开放社区）',
+        deviceinfo: "设备资源感知: 对您的智能设备可用资源进行测评",
+        modelinfo: "模型性能评估: 对您的深度学习模型性能进行评估",
+        modelcompress:
+          "模型压缩: 减小模型大小以及减少模型参数数量，从而提高模型的效率和性能",
+        mdoelseg:
+          "模型分割: 将深度学习模型划分成若干子模型，以便在多个设备运行，从而减少计算和存储资源的需求",
+      },
+      // backgroundImageUrl: '/images/background.jpeg'
+      backgroundImageUrl: "/images/background2.jpg",
+      // backgroundImageUrl: "/images/background4.png",
+    };
+  },
+  directives: { "b-tooltip": VBTooltip },
+};
+</script>
+
+
+<style>
+.background-image {
+  background-size: cover;
+  /* background-position: center center; */
+  background-repeat: no-repeat;
+  height: 300vh;
+  background-attachment: fixed;
+  opacity: 0.95;
+}
+
+.image-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  justify-content: left;
+  align-items: center;
+  height: 140vh;
+  width: 200vh;
+
+  margin: 0 auto;
+  overflow: hidden; /* 清除浮动 */
+}
+
+.image-container .image {
+  order: 1;
+  float: left;
+  width: 500px;
+  margin-right: 20px;
+}
+
+.image-first {
+  display: flex;
+  justify-content: right;
+  width: 80%;
+}
+
+.image-container .text {
+  order: 2;
+  float: right;
+  overflow: hidden;
+  font-weight: bold;
+  color: white;
+  font-size: 20px;
+}
+
+.bold-header {
+  /* font-weight: bold; */
+  color: rgb(255, 0, 55);
+}
+
+.row {
+  display: flex;
+  width: 80%;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  cursor: pointer;
+}
+.hover-animation {
+  transition: transform 0.5s;
+  
+}
+
+.hover-animation:hover {
+  transform: scale(1.1);
+}
+
+.small-image {
+  max-width: 67%;
+  max-height: 67%;
+  margin-top: 3em;
+  margin-bottom: 2em;
+}
+
+
+</style>
