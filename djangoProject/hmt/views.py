4b861839dd26be208dbc6564f77fddae7b0121b9
unknown <1924582937@qq.com>
2023-06-29 20:23:06 +0800

    update scp

--- djangoProject/hmt/views.py
+++ djangoProject/hmt/views.py
@@ -39,6 +39,7 @@ from uploadusermodel.checkmodel_util import model_user
 
 from Luohao.optimation import readdata
 from Luohao.exe.scp import scp_send_files
+from Luohao.exe.scp import scp_send_files
 
 # from hmt.views.nodegraph import optimal  #路径必须这么写才行,django的根目录开始，默认从django的根目录开始识别
 # Create your views here.
